128d3417e68866b96df3cecc7500b4ea4505f75e
srinu <srinivasareddykothapuli@gmail.com>
2025-08-11 09:29:10 +0530

    11-08

---
+++ service-registery-1/src/main/java/com/example/demo/ServiceRegistery1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistery1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistery1Application.class, args);
+	}
+
+}
